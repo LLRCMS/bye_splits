c301172743a412049c69ef37bcfbe550ef91ddd2
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-03-15 15:36:37 +0100

    stable training with unknown results so far

--- optimize_tc_distribution.py
+++ optimize_tc_distribution.py
@@ -1,4 +1,5 @@
 import h5py
+import datetime
 import numpy as np
 import tensorflow as tf
 print("TensorFlow version:", tf.__version__)
@@ -27,87 +28,100 @@ def tensorflow_wasserstein_1d_loss(indata, outdata):
     loss = tf.abs( loss )
     return tf.reduce_sum( loss )
 
-class DataProcessor():
+def data_preprocess(data, nbins_phi, nbins_rz, window_size):
     """Prepare the data to serve as input to the net in R/z slices"""
-    def __init__(self, data, nbinsPhi, nbinsRz, window_size):
-        # data variables' indexes
-        assert data.attrs['columns'].tolist() == ['Rz', 'phi', 'Rz_bin', 'phi_bin']
+    # data variables' indexes
+    assert data.attrs['columns'].tolist() == ['Rz', 'phi', 'Rz_bin', 'phi_bin']
 
-        self.rz_idx = 0
-        self.phi_idx = 1
-        self.rzbin_idx = 2
-        self.phibin_idx = 3
+    rz_idx = 0
+    phi_idx = 1
+    rzbin_idx = 2
+    phibin_idx = 3
 
-        self.data = data[()] #eager (lazy is the default)
-        self.nbins = (nbinsPhi, nbinsRz)
-        self.bound_cond_width = 3
+    data = data[()] #eager (lazy is the default)
 
-        # data preprocessing
-        self._normalize(index=self.phi_idx)
-        self._split(split_index=self.rzbin_idx, sort_index=self.phibin_idx)
-
-        # add cyclic boundaries
-        self.data_with_boundaries, self.boundary_sizes = self.set_boundary_conditions(window_size)
-
-        # drop unneeded columns
-        self._drop_columns(idxs=[self.rz_idx, self.rzbin_idx])
-
-    def _drop_columns(self, idxs):
+    def _drop_columns(data, data_with_boundaries, idxs):
         """Drops the columns specified by indexes `idxs`, overriding data arrays."""
         drop = lambda d,obj: np.delete(d, obj=obj, axis=1)
-        self.data = [drop(x, idxs) for x in self.data]
-        self.data_with_boundaries = [drop(x, idxs) for x in self.data_with_boundaries]
+        data = [drop(x, idxs) for x in data]
+        data_with_boundaries = [drop(x, idxs) for x in data_with_boundaries]
+        return data, data_with_boundaries
 
-    def _split(self, split_index, sort_index):
+    def _split(data, split_index, sort_index, nbins_rz):
         """
         Creates a list of R/z slices, each ordered by phi.
         """
-        self.data = self.data.astype('float32')
+        data = data.astype('float32')
 
         # data sanity check
-        rz_slices = np.unique(self.data[:,split_index])
-        assert len(rz_slices) <= self.nbins[1]
+        rz_slices = np.unique(data[:,split_index])
+        assert len(rz_slices) <= nbins_rz
         assert rz_slices.tolist() == [x for x in range(len(rz_slices))]
 
         # https://stackoverflow.com/questions/2828059/sorting-arrays-in-numpy-by-column
         # ordering is already done when the data is produced, the following line is not needed anymore
-        # self.data = self.data[ self.data[:,split_index].argsort(kind='stable') ] # sort rows by Rz_bin "column"
+        # data = data[ data[:,split_index].argsort(kind='stable') ] # sort rows by Rz_bin "column"
 
         # https://stackoverflow.com/questions/31863083/python-split-numpy-array-based-on-values-in-the-array
 
         # `np.diff` catches all `data` indexes where the sorted bin changes
-        self.data = np.split( self.data, np.where(np.diff(self.data[:,sort_index])<0)[0]+1 )
-        assert len(self.data) == len(rz_slices)
+        data = np.split( data, np.where(np.diff(data[:,sort_index])<0)[0]+1 )
+        assert len(data) == len(rz_slices)
 
         # data correct sorting check
-        for elem in self.data:
+        for elem in data:
             assert ( np.sort(elem[:,sort_index], kind='stable') == elem[:,sort_index] ).all()
 
+        return data
 
-    def _normalize(self, index):
+    def _normalize(data, index):
         """
         Standard max-min normalization of column `index`.
         """
-        ref = self.data[:,index]
+        ref = data[:,index]
         ref = (ref-ref.min()) / (ref.max()-ref.min())
-        
+        return data
 
-    def set_boundary_conditions(self, window_size):
+    def _set_boundary_conditions(data, window_size, phibin_idx, nbins_phi):
         """
         Pad the original data to ensure boundary conditions over its Phi dimension.
         The boundary is done in terms of bins, not single trigger cells.
-        `self.bound_cond_width` stands for the number of bins seen to the right and left.
+        `bound_cond_width` stands for the number of bins seen to the right and left.
         The right boundary is concatenated on the left side of `data`.
         """
         bound_cond_width = window_size - 1
-        boundary_right_indexes = [ (x[:,self.phibin_idx] >= self.nbins[0]-bound_cond_width)
-                                   for x in self.data ]
-        boundary_right = [ x[y] for x,y in zip(self.data,boundary_right_indexes) ]
+        boundary_right_indexes = [ (x[:,phibin_idx] >= nbins_phi-bound_cond_width)
+                                   for x in data ]
+        boundary_right = [ x[y] for x,y in zip(data,boundary_right_indexes) ]
         boundary_sizes = [ len(x) for x in boundary_right ]
         data_with_boundaries = [ np.concatenate((br,x), axis=0)
-                                 for br,x in zip(boundary_right,self.data) ]
+                                 for br,x in zip(boundary_right,data) ]
 
-        return data_with_boundaries, boundary_sizes
+        return data, data_with_boundaries, boundary_sizes
+
+    data = _normalize(
+        data,
+        index=phi_idx
+    )
+    data = _split(
+        data,
+        split_index=rzbin_idx,
+        sort_index=phibin_idx,
+        nbins_rz=nbins_rz
+    )
+    data, data_with_boundaries, boundary_sizes = _set_boundary_conditions(
+        data,
+        window_size,
+        phibin_idx,
+        nbins_phi
+    )
+    data, data_with_boundaries = _drop_columns(
+        data,
+        data_with_boundaries,
+        idxs=[rz_idx, rzbin_idx]
+    )
+
+    return data, data_with_boundaries, boundary_sizes
 
 # https://www.tensorflow.org/guide/keras/custom_layers_and_models#the_model_class
 class Architecture(tf.keras.Model):
@@ -148,30 +162,45 @@ class Architecture(tf.keras.Model):
 
 class TriggerCellDistributor(tf.Module):
     """Neural net workings"""
-    def __init__(self, inshape, kernel_size, window_size,
-                 phibounds, nbinsphi, rzbounds, nbinsrz,
-                 pars=(0.2, 1., 1.)):
+    def __init__( self, indata, inbins, bound_size,
+                  kernel_size, window_size,
+                  phibounds, nbinsphi, rzbounds, nbinsrz,
+                  pars=(1., 1., 1.) ):
         """
         Manages quantities related to the neural model being used.
-        Args: - inshape: Shape of the input data
+        Args: 
               - kernel_size: Length of convolutional kernels
               - window_size: Number of bins considered for each variance calculation.
               Note this is not the same as number of trigger cells (each bin has
               multiple trigger cells).
-        """
-        self.architecture = Architecture(inshape, kernel_size)
+        """        
+        self.indata = indata
+        self.boundary_size = bound_size
         self.kernel_size = kernel_size
         self.boundary_width = window_size-1
         self.phibounds, self.nbinsphi = phibounds, nbinsphi
         self.rzbounds, self.nbinsrz = rzbounds, nbinsrz
 
+        self.architecture = Architecture(self.indata.shape, kernel_size)
+
         assert len(pars)==3
         self.pars = pars
+
+        self.subtract_max = lambda x: x - (tf.math.reduce_max(x)+1)
+        # convert bin ids coming before 0 (shifted boundaries) to negative ones
+        self.inbins = tensorflow_assignment( tensor=inbins,
+                                             mask=tf.concat((tf.ones(self.boundary_size),
+                                                             tf.zeros(inbins.shape[0]-self.boundary_size)), axis=0),
+                                             lambda_op=self.subtract_max,
+                                            )
+        
+        self.indata_variance = self._calc_local_variance(self.indata, self.inbins)
+        self.initial_wasserstein_distance = None
 
         self.optimizer = tf.keras.optimizers.Adam()
         self.train_loss = tf.keras.metrics.Mean(name='train_loss')
 
-    def calc_loss(self, indata, inbins, bound_size, outdata):
+    def calc_loss(self, outdata):
         """
         Calculates the model's loss function. Receives slices in R/z as input.
         Each array value corresponds to a trigger cell.
@@ -181,9 +210,9 @@ class TriggerCellDistributor(tf.Module):
               -outdata: neural net phi values output
         """
         opt = {'summarize': 10} #number of entries to print if assertion fails
-        asserts = [ tf.debugging.Assert(indata.shape == outdata.shape, [indata.shape, outdata.shape], **opt) ]
-        asserts.append( tf.debugging.Assert(tf.math.reduce_min(inbins)==0, [inbins], **opt) )
-        asserts.append( tf.debugging.Assert(tf.math.reduce_max(inbins)==self.nbinsphi-1, [inbins], **opt) )
+        asserts = [ tf.debugging.Assert(self.indata.shape == outdata.shape, [self.indata.shape, outdata.shape], **opt) ]
+        asserts.append( tf.debugging.Assert(tf.math.reduce_min(self.inbins)==-self.boundary_width, [self.inbins], **opt) )
+        asserts.append( tf.debugging.Assert(tf.math.reduce_max(self.inbins)==self.nbinsphi-1, [self.inbins], **opt) )
 
         # bin the output of the neural network
         outbins = tf.histogram_fixed_width_bins(outdata,
@@ -196,40 +225,46 @@ class TriggerCellDistributor(tf.Module):
         # convert bin ids coming before 0 (shifted boundaries) to negative ones
         # if this looks convoluted, blame tensorflow, which still does not support tensor assignment
         # a new tensor must be created instead
-        subtract_max = lambda x: x - (tf.math.reduce_max(x)+1)
-        inbins = tensorflow_assignment( tensor=inbins,
-                                        mask=tf.concat((tf.ones(bound_size),
-                                                        tf.zeros(inbins.shape[0]-bound_size)), axis=0),
-                                        lambda_op=subtract_max,
-                                       )
         outbins = tensorflow_assignment( tensor=outbins,
-                                         mask=tf.concat((tf.ones(bound_size),
-                                                         tf.zeros(outbins.shape[0]-bound_size)), axis=0),
-                                         lambda_op=subtract_max,
+                                         mask=tf.concat((tf.ones(self.boundary_size),
+                                                         tf.zeros(outbins.shape[0]-self.boundary_size)), axis=0),
+                                         lambda_op=self.subtract_max,
                                        )
 
         # replicated boundaries should be the same
         asserts.append( tf.debugging.Assert(
-            condition=are_tensors_equal(indata[:bound_size], indata[-bound_size:]),
-            data=[indata[:bound_size],indata[-bound_size:]],
+            condition=are_tensors_equal(self.indata[:self.boundary_size], self.indata[-self.boundary_size:]),
+            data=[self.indata[:self.boundary_size],self.indata[-self.boundary_size:]],
             **opt) )
         
         with tf.control_dependencies(asserts):
-            # calculate the variance between adjacent bins
-            variance_loss = 0
-            for ibin in tf.unique(outbins)[:-self.boundary_width]:
-                idxs = (outbins >= ibin) & (outbins <= ibin+self.boundary_width)
-                variance_loss += tf.math.reduce_variance(indata[idxs])
+            variance_loss = self._calc_local_variance(outdata, outbins)
 
-            wasserstein_loss = tensorflow_wasserstein_1d_loss(indata, outdata)
+            wasserstein_loss = tensorflow_wasserstein_1d_loss(self.indata, outdata)
+            if self.initial_wasserstein_distance is None: #the first time
+               self.initial_wasserstein_distance = wasserstein_loss
+            wasserstein_loss *= (self.indata_variance/self.initial_wasserstein_distance)
 
-            boundary_sanity_loss = tf.abs( outdata[-bound_size:] - outdata[:bound_size] )
+            boundary_sanity_loss = tf.reduce_sum(
+                tf.abs( outdata[-self.boundary_size:] - outdata[:self.boundary_size] ) )
 
             loss_pars = [ tf.Variable(x, trainable=False) for x in self.pars ]
 
-        return ( loss_pars[0] * variance_loss +
-                 loss_pars[1] * wasserstein_loss +
-                 loss_pars[2] * boundary_sanity_loss )
+        return ( { 'local_variance_loss': loss_pars[0] * variance_loss,
+                   'wasserstein_loss':    loss_pars[1] * wasserstein_loss,
+                   'boundary_loss':       loss_pars[2] * boundary_sanity_loss },
+                 self.indata_variance,
+                 tf.unique(outbins[:-self.boundary_size])[0]
+                 )
+
+    def _calc_local_variance(self, data, bins):
+        """Calculates the variance between adjacent bins."""
+        variance_loss = 0
+        unique_bins = tf.unique(bins[:-self.boundary_size])[0]
+        for ibin in unique_bins:
+            idxs = (bins >= ibin) & (bins <= ibin+self.boundary_width)
+            variance_loss += tf.math.reduce_variance(data[idxs])
+        return variance_loss
 
     # @tf.function(
     #     input_signature=(
@@ -237,21 +272,24 @@ class TriggerCellDistributor(tf.Module):
     #         tf.TensorSpec(shape=[], dtype=tf.int32))
     # )
     #@tf.function
-    def train(self, indata, inbins, bound_size):
+    def train(self):
         # Reset the metrics at the start of the next epoch
         self.train_loss.reset_states()
 
         # for n in tf.range(steps): # any future loop within one epoch is done here
         with tf.GradientTape() as tape:
-            tape.watch(indata)
-            tape.watch(inbins)
+            tape.watch(self.indata)
+            tape.watch(self.inbins)
 
-            prediction = self.architecture(indata)
-            loss = self.calc_loss(indata, inbins, bound_size, prediction)
-        gradients = tape.gradient(loss, self.architecture.trainable_variables)
+            prediction = self.architecture(self.indata)
+            losses, initial_variance, _ = self.calc_loss(prediction)
+            loss_sum = tf.reduce_sum(list(losses.values()))
+
+        gradients = tape.gradient(loss_sum, self.architecture.trainable_variables)
+
         self.optimizer.apply_gradients(zip(gradients, self.architecture.trainable_variables))
-        self.train_loss(loss)
-        return loss, prediction
+        self.train_loss(loss_sum)
+        return losses, initial_variance, prediction
 
     def plot(self, name):
         """Plots the structure of the used architecture."""
@@ -267,41 +305,59 @@ class TriggerCellDistributor(tf.Module):
             dpi=96,
         )
 
+def save_scalar_logs(writer, scalar_map, epoch):
+    """
+    Saves tensorflow info for Tensorboard visualization.
+    `scalar_map` expects a dict of (scalar_name, scalar_value) pairs.
+    """
+    with writer.as_default():
+        for k,v in scalar_map.items():
+            tf.summary.scalar(k, v, step=epoch)
+
 def optimization(algo, **kw):
     store_in  = h5py.File(kw['OptimizationIn'],  mode='r')
     #store_out = h5py.File(kw['OptimizationOut'], mode='w')
 
     assert len(store_in.keys()) == 1
-    train_data_raw = DataProcessor( store_in['data'],
-                                    kw['NbinsPhi'], kw['NbinsRz'],
-                                    kw['WindowSize'] )
+    _, train_data, boundary_sizes = data_preprocess(
+        data=store_in['data'],
+        nbins_phi=kw['NbinsPhi'],
+        nbins_rz=kw['NbinsRz'],
+        window_size=kw['WindowSize']
+    )
 
-    train_data = train_data_raw.data_with_boundaries
-    boundary_sizes = train_data_raw.boundary_sizes
-
-    #train_data = tf.data.Dataset.from_tensor_slices( train_data_raw )
+    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    train_log_dir = 'logs/gradient_tape/' + current_time + '/train'
 
     for i,rzslice in enumerate(train_data):
-        if i>0:
+        if i>0:        #look at the first R/z slice only
             break
-        #look at the first R/z slice only
-        indata = tf.convert_to_tensor(rzslice[:,0], dtype=tf.float32)
-        inbins = tf.convert_to_tensor(rzslice[:,1], dtype=tf.float32)
+        summary_writer = tf.summary.create_file_writer(train_log_dir)
 
-        tcd = TriggerCellDistributor( inshape=indata.shape,
-                                      kernel_size=kw['KernelSize'],
-                                      window_size=kw['WindowSize'],
-                                      phibounds=(kw['MinPhi'],kw['MaxPhi']),
-                                      nbinsphi=kw['NbinsPhi'],
-                                      rzbounds=(kw['MinROverZ'],kw['MaxROverZ']),
-                                      nbinsrz=kw['NbinsRz'] )
+        tcd = TriggerCellDistributor(
+            indata=tf.convert_to_tensor(rzslice[:,0], dtype=tf.float32),
+            inbins=tf.convert_to_tensor(rzslice[:,1], dtype=tf.float32),
+            bound_size=boundary_sizes[i],
+            kernel_size=kw['KernelSize'],
+            window_size=kw['WindowSize'],
+            phibounds=(kw['MinPhi'],kw['MaxPhi']),
+            nbinsphi=kw['NbinsPhi'],
+            rzbounds=(kw['MinROverZ'],kw['MaxROverZ']),
+            nbinsrz=kw['NbinsRz'],
+            pars=(3., 1., 1.),
+        )
         tcd.plot('model{}.png'.format(i))
 
         for epoch in range(kw['Epochs']):
-            loss, out_dist = tcd.train(indata, inbins, boundary_sizes[i])
+            dictloss, initial_variance, _ = tcd.train()
+            dictloss.update({'initial_variance': initial_variance})
+            save_scalar_logs(
+                writer=summary_writer,
+                scalar_map=dictloss,
+                epoch=epoch
+            )
             print('Epoch {}, Loss: {}'.format(epoch+1, tcd.train_loss.result()))
 
-            
 if __name__ == "__main__":
     from airflow.airflow_dag import optimization_kwargs
     for falgo in optimization_kwargs['FesAlgos']:
