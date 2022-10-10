ec18af71e053cdefe7fabfc63531f2a892088572
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-10-10 15:36:28 +0200

    use system's python

--- bye_splits/run_iterative_optimization.sh
+++ bye_splits/run_iterative_optimization.sh
@@ -185,7 +185,7 @@ fi
 
 ### Functions
 function run_parallel() {
-	comm="parallel -j -1 python3 iterative_optimization.py --ipar {} --sel ${SELECTION} -n ${NEVENTS} --reg ${REGION} "
+	comm="parallel -j -1 python iterative_optimization.py --ipar {} --sel ${SELECTION} -n ${NEVENTS} --reg ${REGION} "
 	comm+="--cluster_algo ${CLUSTER_ALGO} --seed_window ${SEED_WINDOW} --smooth_kernel ${SMOOTH_KERNEL} "
 	if [ ${DO_FILLING} -eq 0 ]; then
 		echo "Do not run the filling step."
