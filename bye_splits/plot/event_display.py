d5b89a261381373ac098f3cf97f2bce09ad2e90e
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-10-25 14:17:33 +0200

    2d square histogram added on the right

--- bye_splits/plot/event_display.py
+++ bye_splits/plot/event_display.py
@@ -14,13 +14,34 @@ import uproot as up
 from bokeh.io import output_file, save
 from bokeh.plotting import figure
 from bokeh.util.hex import axial_to_cartesian
-from bokeh.models import Panel, Tabs, BoxZoomTool, Range1d
+from bokeh.models import (
+    Panel,
+    Tabs,
+    BoxZoomTool,
+    Range1d,
+    ColumnDataSource,
+    HoverTool,
+    )
+from bokeh.layouts import layout
 output_file('tmp.html')
 
 import utils
 from utils import params, common, parsing
 
 flatten = lambda col : col.to_numpy().squeeze()
+
+def common_props(p, xlim=None, ylim=None):
+    p.output_backend = 'svg'
+    p.toolbar.logo = None
+    p.grid.visible = False
+    p.outline_line_color = None
+    p.xaxis.visible = False
+    p.yaxis.visible = False
+    if xlim is not None:
+        p.x_range = Range1d(xlim[0], xlim[1])
+    if ylim is not None:
+        p.y_range = Range1d(ylim[0], ylim[1])
+
 
 tc_path = ( Path(__file__).parent.absolute().parent.parent /
             params.DataFolder / 'test_triggergeom.root' )
@@ -34,6 +55,8 @@ tc_data = tc_tree.arrays({var1, var2, *sel_vars}, library='pd')
 layers = flatten(tc_data[['layer']].drop_duplicates().sort_values('layer'))
 
 tabs_uv, figs_uv = ([] for _ in range(2))
+tabs_xy, figs_xy = ([] for _ in range(2))
+
 for il in layers:
     l_data = tc_data[(tc_data.layer==il) & (tc_data.zside==1) & (tc_data.subdet==1)][:]
     l_data = l_data[[var1, var2]].drop_duplicates().sort_values([var1, var2])
@@ -41,27 +64,46 @@ for il in layers:
     u = flatten(l_data[var1])
     v = flatten(l_data[var2])
     v *= -1 #convert between CMSSW coordinate system and bokeh's
-     
+    xcart, ycart = axial_to_cartesian(u, v, 1, 'pointytop')
+    source = ColumnDataSource({'u': u, 'v': v, 'xcart': xcart, 'ycart': ycart,
+                               'color': ['firebrick']*len(u)})
+
+    # (u,v) plots
     figs_uv.append(figure(width=800, height=800,
                           tools='save,reset', toolbar_location='right'))
     figs_uv[-1].add_tools(BoxZoomTool(match_aspect=True))
-    figs_uv[-1].output_backend = 'svg'
-    figs_uv[-1].toolbar.logo = None
-    figs_uv[-1].grid.visible = False
-    figs_uv[-1].outline_line_color = None
-    figs_uv[-1].x_range=Range1d(-20, 20)
-    figs_uv[-1].y_range=Range1d(-20, 20)
-    figs_uv[-1].xaxis.visible = False
-    figs_uv[-1].yaxis.visible = False
-     
-    figs_uv[-1].hex_tile(u, v, size=1, fill_color=['firebrick']*len(u),
+    
+    common_props(figs_uv[-1], xlim=(-20,20), ylim=(-20,20))
+    figs_uv[-1].hex_tile(q='u', r='v', source=source,
+                         size=1, fill_color='color',
                          line_color='black', line_width=1, alpha=1.)
      
-    x, y = axial_to_cartesian(u, v, 1, 'pointytop')
-     
-    figs_uv[-1].text(x, y, text=['{}/{}'.format(u,v) for (u, v) in zip(u, v)],
-           text_baseline='middle', text_align='center',
-           text_font_size='7pt')
+    figs_uv[-1].add_tools(HoverTool(tooltips=[('u/v', '@u/@v)'),]))
+
+    # figs_uv[-1].text(xcart, ycart, text=['{}/{}'.format(u,v) for (u, v) in zip(u, v)],
+    #                  text_baseline='middle', text_align='center',
+    #                  text_font_size='7pt')
     tabs_uv.append(Panel(child=figs_uv[-1], title=str(il)))
+
+    # (x,y) plots
+    figs_xy.append(figure(width=800, height=800,
+                          tools='save,reset', toolbar_location='right'))
+    figs_xy[-1].add_tools(BoxZoomTool(match_aspect=True))
+    figs_xy[-1].add_tools(HoverTool(tooltips=[('u/v', '@u/@v'),],))
     
-save(Tabs(tabs=tabs_uv))
+    common_props(figs_xy[-1], xlim=(-13,13), ylim=(-13,13))
+    figs_xy[-1].rect(x='u', y='v',
+                     source=source,
+                     width=1., height=1.,
+                     width_units='data', height_units='data',
+                     fill_color='color',
+                     line_color='black',)
+     
+    #x, y = axial_to_cartesian(u, v, 1, 'pointytop') 
+    # figs_xy[-1].text(u, v, text=['{}/{}'.format(u,v) for (u, v) in zip(u, v)],
+    #                  text_baseline='middle', text_align='center',
+    #                  text_font_size='7pt')
+    tabs_xy.append(Panel(child=figs_xy[-1], title=str(il)))
+
+lay = layout([[Tabs(tabs=tabs_uv),Tabs(tabs=tabs_xy)]])
+save(lay)
