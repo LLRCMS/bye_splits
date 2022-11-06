5864e50438553dda6d633465ddf7b2eebfd7cf6c
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-11-06 23:22:38 +0100

    :construction: plotly 3d scatter test

---
+++ bye_splits/plot/event_display_3d.py
@@ -0,0 +1,32 @@
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H4('Iris XXXX filtered by petal width'),
+    dcc.Graph(id="graph"),
+    html.P("Petal Width:"),
+    dcc.RangeSlider(
+        id='range-slider',
+        min=0, max=2.5, step=0.1,
+        marks={0: '0', 2.5: '2.5'},
+        value=[0.5, 2]
+    ),
+])
+
+@app.callback(
+    Output("graph", "figure"), 
+    Input("range-slider", "value"))
+def update_bar_chart(slider_range):
+    df = px.data.iris() # replace with your own data source
+    low, high = slider_range
+    mask = (df.petal_width > low) & (df.petal_width < high)
+
+    fig = px.scatter_3d(df[mask], 
+        x='sepal_length', y='sepal_width', z='petal_width',
+        color="species", hover_data=['petal_width'])
+    return fig
+
+app.run_server(debug=True,
+               port=8003)
