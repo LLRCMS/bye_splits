24e4b6b2ea34b4de012b2b8cdbcb3bf5aa9c76cd
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-11-11 10:25:56 +0100

    :bricks: introduce data base class

---
+++ bye_splits/data_handle/base.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+
+_all_ = [ ]
+
+import os
+from pathlib import Path
+import sys
+parent_dir = os.path.abspath(__file__ + 2 * '/..')
+sys.path.insert(0, parent_dir)
+
+import abc
+import pandas as pd
+
+from utils import params, common
+
+class BaseData(abc.ABC):
+    def __init__(self, inname, outname):
+        self.inpath = (Path(__file__).parent.absolute().parent.parent /
+                       params.DataFolder / inname )
+        self.outpath = (Path(__file__).parent.absolute().parent.parent /
+                        params.DataFolder / outname )
+        self.dname = 'tc'
+        self.var = common.dot_dict({'u': 'waferu', 'v': 'waferv', 'l': 'layer',
+                                    'x': 'x', 'y': 'y', 'z': 'z',
+                                    'side': 'zside', 'subd': 'subdet'})
+        self.newvar = common.dot_dict({'vs': 'waferv_shift', 'c': 'color'})
+
+    def provide(self, reprocess=False):
+        if not os.path.exists(self.outpath) or reprocess:
+            self.store()
+        with pd.HDFStore(self.outpath, mode='r') as s:
+            res = s[self.dname]
+        return res
+
+    @abc.abstractmethod
+    def select(self):
+        raise NotImplementedError()
+    
+    def store(self):
+        data = self.select()
+        with pd.HDFStore(self.outpath, mode='w') as s:
+            s[self.dname] = data
+
+    def variables(self):
+        res = self.var.copy()
+        res.update(self.newvar)
+        return res
--- bye_splits/data_handle/geometry.py
+++ bye_splits/data_handle/geometry.py
@@ -9,29 +9,14 @@ parent_dir = os.path.abspath(__file__ + 2 * '/..')
 sys.path.insert(0, parent_dir)
 
 import uproot as up
-import pandas as pd
 
-from utils import params, common
+from utils import params
+from data_handle.base import BaseData
 
-class GeometryData:
+class GeometryData(BaseData):
     def __init__(self, inname, outname):
-        self.inpath = (Path(__file__).parent.absolute().parent.parent /
-                     params.DataFolder / inname )
-        self.outpath = (Path(__file__).parent.absolute().parent.parent /
-                        params.DataFolder / outname )
-        self.dname = 'tc'
-        self.var = common.dot_dict({'u': 'waferu', 'v': 'waferv', 'l': 'layer',
-                                    'x': 'x', 'y': 'y', 'z': 'z',
-                                    'side': 'zside', 'subd': 'subdet'})
-        self.newvar = common.dot_dict({'vs': 'waferv_shift', 'c': 'color'})
-
-    def provide(self, reprocess=False):
-        if not os.path.exists(self.outpath) or reprocess:
-            self.store()
-        with pd.HDFStore(self.outpath, mode='r') as s:
-            res = s[self.dname]
-        return res
-
+        super().__init__(inname, outname)
+        
     def select(self):
         with up.open(self.inpath) as f:
             tree = f[ os.path.join('hgcaltriggergeomtester', 'TreeTriggerCells') ]
@@ -46,13 +31,3 @@ class GeometryData:
             data[self.newvar.c] = "#8a2be2"
             
         return data
-
-    def store(self):
-        data = self.select()
-        with pd.HDFStore(self.outpath, mode='w') as s:
-            s[self.dname] = data
-
-    def variables(self):
-        res = self.var.copy()
-        res.update(self.newvar)
-        return res
