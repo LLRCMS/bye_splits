cd83729fbb06732a6689355971de1a38eb8bce2a
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-11-15 14:43:25 +0100

    specify websocket

--- app.sh
+++ app.sh
@@ -1,1 +1,1 @@
-bokeh serve bye_splits/plot/display/ --address 0.0.0.0 --port 8080
+bokeh serve bye_splits/plot/display/ --address 0.0.0.0 --port 8080 --allow-websocket-origin=viz2-hgcal-event-display.app.cern.ch
