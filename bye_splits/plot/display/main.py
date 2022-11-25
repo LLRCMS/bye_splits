a9841bb095586b8806c28aa8f48accfbb25b0dac
bfontana <20703947+b-fontana@users.noreply.github.com>
2022-11-25 08:58:28 +0100

    add color bar

--- bye_splits/plot/display/main.py
+++ bye_splits/plot/display/main.py
@@ -1,3 +1,4 @@
+
 # coding: utf-8
 
 _all_ = [ ]
@@ -34,7 +35,16 @@ from bokeh.models import (
     CustomJSFilter,
     CDSView,
     WheelZoomTool,
+    BasicTicker,
+    ColorBar, 
+    LogColorMapper,
+    LogTicker,
+    LinearColorMapper,
+    BasicTicker,
+    PrintfTickFormatter,
     )
+from bokeh.palettes import viridis as _palette
+mypalette = _palette(50)
 from bokeh.layouts import layout
 from bokeh.settings import settings
 settings.ico_path = 'none'
@@ -43,6 +53,10 @@ import utils
 from utils import params, common, parsing
 import data_handle
 from data_handle.data_handle import handle
+data_vars = {'ev': handle('event').variables,
+             'geom': handle('geom').variables,
+             }
+mode = 'geom'
 
 def common_props(p, xlim=None, ylim=None):
     p.output_backend = 'svg'
@@ -57,15 +71,16 @@ def common_props(p, xlim=None, ylim=None):
         p.y_range = Range1d(ylim[0], ylim[1])
         
 def convert_cells_to_xy(df, avars):
-    c30, s30 = np.sqrt(3)/2, 1/2
-    d, d4 = 1./c30, 1./c30
+    c30, s30, t30 = np.sqrt(3)/2, 1/2, 1/np.sqrt(3)
+    d = 1.#1./c30
+    d4, d8 = d/4., d/8.
     conversion = {
-        'UL': (lambda wu,wv,cv: 2*wu - wv + c30 * d4 * cv,
-               lambda wv,cu,cv: c30*wv + s30 * d4 * (2*(cu-1)-cv)),
-        'UR': (lambda wu,wv,cv: 2*wu - wv + 4*c30 + c30 * d4 * (cv-4),
-               lambda wv,cu,cv: c30*wv + 2 + s30 * d4 * (2*(cu-4)-(cv-4))),
-        'B':  (lambda wu,wv,cv: 2*wu - wv + c30 * d4 * cv,
-               lambda wv,cu,cv: c30*wv + s30 * d4 * (2*cu-cv))
+        'UL': (lambda wu,wv,cv: 2*wu - wv + d4 * cv,
+               lambda wv,cu,cv: ((d/c30)+d*t30)*wv + d8/c30 * (2*(cu-1)-cv)),
+        'UR': (lambda wu,wv,cv: 2*wu - wv + d + d4 * (cv-4),
+               lambda wv,cu,cv: ((d/c30)+d*t30)*wv + t30*d + d8/c30 * (2*(cu-4)-(cv-4))),
+        'B':  (lambda wu,wv,cv: 2*wu - wv + d4 * cv,
+               lambda wv,cu,cv: ((d/c30)+d*t30)*wv + t30 * d4 * (2*cu-cv))
     } #up-right, up-left and bottom
 
     masks = {'UL': (((df[avars['cu']]>=1) & (df[avars['cu']]<=4) & (df[avars['cv']]==0)) |
@@ -84,29 +99,29 @@ def convert_cells_to_xy(df, avars):
         x0.update({key: conversion[key][0](df[avars['wu']][masks[key]],
                                            df[avars['wv']][masks[key]],
                                            df[avars['cv']][masks[key]])})
-        x1.update({key: x0[key][:] + c30})
+        x1.update({key: x0[key][:] + d4})
         if key in ('UL', 'UR'):
             x2.update({key: x1[key][:]})
             x3.update({key: x0[key][:]})
         else:
-            x2.update({key: x1[key][:] + c30})
+            x2.update({key: x1[key][:] + d4})
             x3.update({key: x1[key][:]})
      
         y0.update({key: conversion[key][1](df[avars['wv']][masks[key]],
                                            df[avars['cu']][masks[key]],
                                            df[avars['cv']][masks[key]])})
         if key in ('UR', 'B'):
-            y1.update({key: y0[key][:] - s30})
+            y1.update({key: y0[key][:] - t30*d4})
         else:
-            y1.update({key: y0[key][:] + s30})
+            y1.update({key: y0[key][:] + t30*d4})
         if key in ('B'):
             y2.update({key: y0[key][:]})
         else:
-            y2.update({key: y1[key][:] + d})
+            y2.update({key: y1[key][:] + d4/c30})
         if key in ('UL', 'UR'):
-            y3.update({key: y0[key][:] + d})
+            y3.update({key: y0[key][:] + d4/c30})
         else:
-            y3.update({key: y0[key][:] + s30})
+            y3.update({key: y0[key][:] + t30*d4})
 
         xaxis.update({key: pd.concat([x0[key],x1[key],x2[key],x3[key]], axis=1)})
         yaxis.update({key: pd.concat([y0[key],y1[key],y2[key],y3[key]], axis=1)})
@@ -125,40 +140,44 @@ def convert_cells_to_xy(df, avars):
     df.rename(columns = {avars['l']: 'layer'}, inplace=True)
     return df.join(res)
 
-def get_data(particle):
-    event = 179855 if particle == 'photons' else 92004
-    ev_vars = handle('event', particle).variables()
-    geom_vars = handle('geom').variables()
-        
+def get_data(event, particle):
+    #event = 179855 if particle == 'photons' else 92004        
     ds_ev = handle('event', particle).provide_event(event, True)
-    ds_ev = convert_cells_to_xy(ds_ev, ev_vars)
+    ds_ev = convert_cells_to_xy(ds_ev, data_vars['ev'])
     ds_geom = handle('geom').provide(True)
-    ds_geom = convert_cells_to_xy(ds_geom, geom_vars)
-    ds_geom = ds_geom[(ds_geom[geom_vars['wu']]==4) & (ds_geom[geom_vars['wv']]==4) ]
-    return ds_ev, ds_geom 
+    ds_geom = convert_cells_to_xy(ds_geom, data_vars['geom'])
+    ds_geom = ds_geom[((ds_geom[geom_vars['wu']]==4) & (ds_geom[geom_vars['wv']]==4))]
+                       # (ds_geom[geom_vars['wu']]==5) & (ds_geom[geom_vars['wv']]==4) |
+                       # (ds_geom[geom_vars['wu']]==5) & (ds_geom[geom_vars['wv']]==5))]
+    return {'ev': ds_ev, 'geom': ds_geom}
 
-sources = {'photons'   : ColumnDataSource(data=get_data('photons')[1]),
-           'electrons' : ColumnDataSource(data=get_data('electrons')[1]),
+sources = {'photons'   : ColumnDataSource(data=get_data(179855, 'photons')[mode]),
+           'electrons' : ColumnDataSource(data=get_data(92004,  'electrons')[mode]),
            }
 elements = {'photons'   : {'textinput': TextInput(title='Event', value='', sizing_mode='stretch_width')},
             'electrons' : {'textinput': TextInput(title='Event', value='', sizing_mode='stretch_width')},
             }
+
 assert sources.keys() == elements.keys()
 
 def display():
+    variables = data_vars[mode]
     def text_callback(attr, old, new, source, particle):
-        print('running ', particle)
-        source.data = get_data(particle)[1]
+        print('running ', particle, new)
+        assert new.isdecimal()
+        source.data = get_data(new, particle)[mode]
 
     doc = curdoc()
     doc.title = 'TC Visualization'
     
-    width, height = int(1600/3), 400
+    width, height   = 1250, 1000
+    width2, height2 = 400, 300
     tabs = []
     
     for ksrc,vsrc in sources.items():
-        ev_vars = handle('event', ksrc).variables()
-        geom_vars = handle('geom').variables()
+        if mode == 'ev':
+            mapper = LinearColorMapper(palette=mypalette,
+                                       low=vsrc.data[variables['en']].min(), high=vsrc.data[variables['en']].min())
 
         slider = Slider(start=vsrc.data['layer'].min(), end=vsrc.data['layer'].max(),
                         value=vsrc.data['layer'].min(), step=2, title='Layer',
@@ -183,48 +202,71 @@ def display():
         # p_uv.add_tools(WheelZoomTool(),
         #                BoxZoomTool(match_aspect=True))
         # common_props(p_uv, xlim=(-20,20), ylim=(-20,20))
-        # p_uv.hex_tile(q=ev_vars['tcwu'], r=ev_vars['tcwv'], source=vsrc, view=view,
+        # p_uv.hex_tile(q=variables['tcwu'], r=variables['tcwv'], source=vsrc, view=view,
         #               size=1, fill_color='color', line_color='black', line_width=1, alpha=1.)    
-        # p_uv.add_tools(HoverTool(tooltips=[('u/v', '@'+ev_vars['tcwu']+'/'+'@'+ev_vars['tcwv']),]))
+        # p_uv.add_tools(HoverTool(tooltips=[('u/v', '@'+variables['tcwu']+'/'+'@'+variables['tcwv']),]))
 
         ####### cell plots ################################################################
-        polyg_opt = dict(line_color='black', line_width=3)
+        polyg_opt = dict(line_color='black', line_width=1)
+        lim = 22
         p_cells = figure(width=width, height=height,
+                         x_range=Range1d(-lim, lim),
+                         y_range=Range1d(-lim, lim),
                          tools='save,reset', toolbar_location='right',
                          output_backend='webgl')
+        if mode == 'ev':
+            hover_key = 'Energy (wu,wv,cu,cv)'
+            hover_val = '@'+variables['en'] + ' (@'+variables['wu']+',@'+variables['wv']+',@'+variables['cv']+',@'+variables['cv']+')'
+        else:
+            hover_key = '(wu,wv,cu,cv)'
+            hover_val = '(@'+variables['wu']+',@'+variables['wv']+',@'+variables['cv']+',@'+variables['cv']+')'
+
         p_cells.add_tools(BoxZoomTool(match_aspect=True),
-                          HoverTool(tooltips=[('u/v', '@'+geom_vars['cu']+'/'+'@'+geom_vars['cv']),]))
-        p_cells.multi_polygons(xs='tc_polyg_x', ys='tc_polyg_y',
-                               source=vsrc, view=view, color='red',
-                               **polyg_opt)
+                          WheelZoomTool(),
+                          HoverTool(tooltips=[(hover_key, hover_val),]))
+        common_props(p_cells, xlim=(-lim, lim), ylim=(-lim, lim))
+        p_cells_opt = dict(xs='tc_polyg_x', ys='tc_polyg_y',
+                           source=vsrc, view=view, **polyg_opt)
         
+        if mode == 'ev':
+            p_cells.multi_polygons(fill_color={'field': variables['en'], 'transform': mapper},
+                                   **p_cells_opt)
+        else:
+            p_cells.multi_polygons(color='green', **p_cells_opt)
+                        
+        if mode == 'ev':
+            color_bar = ColorBar(color_mapper=mapper,
+                                ticker= BasicTicker(desired_num_ticks=int(len(mypalette)/4)),
+                                formatter=PrintfTickFormatter(format="%d"))
+            p_cells.add_layout(color_bar, 'right')
+
         ####### (x,y) plots ################################################################
         # p_xy = figure(width=width, height=height,
         #             tools='save,reset', toolbar_location='right',
         #             output_backend='webgl')
         # p_xy.add_tools(WheelZoomTool(), BoxZoomTool(match_aspect=True))
-        # p_xy.add_tools(HoverTool(tooltips=[('u/v', '@'+ev_vars['tcwu']+'/'+'@'+ev_vars['tcwv']),],))       
+        # p_xy.add_tools(HoverTool(tooltips=[('u/v', '@'+variables['tcwu']+'/'+'@'+variables['tcwv']),],))       
         # common_props(p_xy, xlim=(-13,13), ylim=(-13,13))
-        # p_xy.rect(x=ev_vars['tcwu'], y=ev_vars['tcwv'], source=vsrc, view=view,
+        # p_xy.rect(x=variables['tcwu'], y=variables['tcwv'], source=vsrc, view=view,
         #           width=1., height=1., width_units='data', height_units='data',
         #           fill_color='color', line_color='black',)
 
-        ####### x vs. z plots ################################################################
-        # p_xVSz = figure(width=width, height=height, tools='save,reset', toolbar_location='right')
+        # ####### x vs. z plots ################################################################
+        # p_xVSz = figure(width=width2, height=height2, tools='save,reset', toolbar_location='right')
         # p_xVSz.add_tools(BoxZoomTool(match_aspect=True))
-        # p_xVSz.scatter(x=ev_vars['z'], y=ev_vars['x'], source=vsrc)
+        # p_xVSz.scatter(x=variables['z'], y=variables['x'], source=vsrc)
         # common_props(p_xVSz)
         
-        ####### y vs. z plots ################################################################
-        # p_yVSz = figure(width=width, height=height, tools='save,reset', toolbar_location='right')
+        # ####### y vs. z plots ################################################################
+        # p_yVSz = figure(width=width2, height=height2, tools='save,reset', toolbar_location='right')
         # p_yVSz.add_tools(BoxZoomTool(match_aspect=True))
-        # p_yVSz.scatter(x=ev_vars['z'], y=ev_vars['y'], source=vsrc)
+        # p_yVSz.scatter(x=variables['z'], y=variables['y'], source=vsrc)
         # common_props(p_yVSz)
         
-        ####### y vs. x plots ################################################################
-        # p_yVSx = figure(width=width, height=height, tools='save,reset', toolbar_location='right')
+        # ####### y vs. x plots ################################################################
+        # p_yVSx = figure(width=width2, height=height2, tools='save,reset', toolbar_location='right')
         # p_yVSx.add_tools(BoxZoomTool(match_aspect=True))
-        # p_yVSx.scatter(x=ev_vars['x'], y=ev_vars['y'], source=vsrc)
+        # p_yVSx.scatter(x=variables['x'], y=variables['y'], source=vsrc)
         # common_props(p_yVSx)
         
         ####### text input ###################################################################
@@ -236,9 +278,9 @@ def display():
 
         lay = layout([[elements[ksrc]['textinput'], blank2, slider],
                       #[p_cells, p_uv, p_xy],
+                      #[p_xVSz, p_yVSz, p_yVSx],
                       [p_cells],
                       [blank1],
-                      # [p_xVSz,p_yVSz,p_yVSx]
                       ])
         tab = TabPanel(child=lay, title=ksrc)
         tabs.append(tab)
@@ -249,32 +291,3 @@ def display():
 parser = argparse.ArgumentParser(description='')
 FLAGS = parser.parse_args()
 display()
-
-# (Pdb) p ds_geom.loc[240635]              
-# waferu                                                         -2
-# waferv                                                         -5
-# layer                                                           5
-# triggercellu                                                    6
-# triggercellv                                                    4
-# x                                                       -7.216201
-# y                                                      -67.298866
-# z                                                      328.042755
-# waferv_shift                                                    5
-# color                                                     #8a2be2
-# tc_polyg_x      [[[2.598076211353316, 3.4641016151377544, 3.46...
-# tc_polyg_y                               [[[4.5, 5.0, 6.0, 5.5]]]
-# Name: 240635, dtype: object
-# (Pdb) p ds_geom.loc[240730]
-# waferu                                                         -4
-# waferv                                                        -11
-# layer                                                           5
-# triggercellu                                                    7
-# triggercellv                                                    5
-# x                                                       -21.82198
-# y                                                     -153.202576
-# z                                                      328.042755
-# waferv_shift                                                   11
-# color                                                     #8a2be2
-# tc_polyg_x      [[[2.598076211353316, 3.4641016151377544, 3.46...
-# tc_polyg_y                               [[[4.5, 5.0, 6.0, 5.5]]]
-# Name: 240730, dtype: object
