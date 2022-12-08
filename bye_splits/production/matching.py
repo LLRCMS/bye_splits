3c369c52d54082ca3c64684b69e873d8419d590f
itehle <itehle@gmail.com>
2022-12-08 18:14:36 +0100

    Preliminaries

--- bye_splits/production/matching.py
+++ bye_splits/production/matching.py
@@ -32,7 +32,7 @@ def matching(event):
         cond_b = event.cl3d_pt==event[cond_a].cl3d_pt.max()
         return (cond_a&cond_b)
 
-def create_dataframes(files, algo_trees, gen_tree, reachedEE):
+def create_dataframes(files, algo_trees, gen_tree, reachedEE, **options):
     #print('Input file: {}'.format(files), flush=True)
     branches_gen = [ 'event', 'genpart_reachedEE', 'genpart_pid', 'genpart_gen',
                      'genpart_exphi', 'genpart_exeta', 'genpart_energy','genpart_pt' ]
@@ -41,18 +41,7 @@ def create_dataframes(files, algo_trees, gen_tree, reachedEE):
     branches_tc = [ 'event', 'tc_zside', 'tc_energy', 'tc_mipPt', 'tc_pt', 'tc_layer',
                     'tc_x', 'tc_y', 'tc_z', 'tc_phi', 'tc_eta', 'tc_id' ]
 
-    # branches_gen = [ 'event', 'genpart_reachedEE', 'genpart_pid', 'genpart_gen',
-    #                  'genpart_exphi', 'genpart_exeta', 'genpart_energy' ]
-    # branches_cl3d = [ 'event', 'cl3d_energy','cl3d_pt','cl3d_eta','cl3d_phi' ]
-    # branches_tc = [ 'event', 'tc_zside', 'tc_energy', 'tc_mipPt', 'tc_pt', 'tc_layer',
-    #                 'tc_x', 'tc_y', 'tc_z', 'tc_phi', 'tc_eta', 'tc_id' ]
-    '''branches_gen = [ 'event', 'good_genpart_exphi', 'good_genpart_exeta', 'good_genpart_energy', 'good_genpart_pt' ]
-    branches_cl3d = [ 'event', 'good_cl3d_energy','good_cl3d_pt','good_cl3d_eta','good_cl3d_phi' ]
-    branches_tc = [ 'event', 'good_tc_energy', 'good_tc_mipPt', 'good_tc_pt', 'good_tc_layer',
-                    'good_tc_x', 'good_tc_y', 'good_tc_z', 'good_tc_phi', 'good_tc_eta' ]'''
-
     batches_gen, batches_tc = ([] for _ in range(2))
-    #memsize_gen, memsize_tc = '128 MB', 50000#'64 MB'
     memsize_gen, memsize_tc = '128 MB', '64 MB'
     for filename in files:
         print('Input file: {}'.format(filename))
@@ -60,13 +49,15 @@ def create_dataframes(files, algo_trees, gen_tree, reachedEE):
             #print( data.num_entries_for(memsize, expressions=branches_tc) )
             for ib,batch in enumerate(data.iterate(branches_gen, step_size=memsize_gen,
                                                    library='pd')):
+                if ib==0:
+                    print('Testing: {}'.format(options['max_num_elements']))
                 batch.set_index('event', inplace=True)
 
                 batches_gen.append(batch)
                 print('Step {}: +{} generated data processed.'.format(ib,memsize_gen), flush=True)
 
             for ib,batch in enumerate(data.iterate(branches_tc, step_size=memsize_tc,
-                                                   library='pandas')):
+                                                   library='pd', max_num_elements=5000)):
                 #batch = batch[ ~batch['good_tc_layer'].isin(params.disconnectedTriggerLayers) ]
                 batch = batch[ ~batch['tc_layer'].isin(params.disconnectedTriggerLayers) ]
                 #convert all the trigger cell hits in each event to a list
@@ -98,11 +89,13 @@ def create_dataframes(files, algo_trees, gen_tree, reachedEE):
     return (df_gen, df_algos, df_tc)
 
 def preprocessing():
-    files = prod_params.files
+    #files = prod_params.files
+    files = ['ntuple_1.root']
     gen, algo, tc = create_dataframes(files,
                                       prod_params.algo_trees, prod_params.gen_tree,
                                       prod_params.reachedEE)
 
+    breakpoint()
     algo_clean = {}
 
     for algo_name,df_algo in algo.items():
