8409cfa5da5a4c6014b6a2f53687e580f74270d7
bfontana <20703947+b-fontana@users.noreply.github.com>
2023-01-06 17:48:14 +0100

    minor

--- bye_splits/plot/join/app.py
+++ bye_splits/plot/join/app.py
@@ -9,10 +9,19 @@ from tornado.ioloop import IOLoop
 import pandas as pd
 import numpy as np
 
-from flask import Flask, render_template
+from flask import Flask, render_template, render_template_string
 app = Flask(__name__)
 
-def bkapp(doc):
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+
+from flask import Flask, render_template_string
+import pandas as pd
+import plotly.express as px
+
+app = Flask(__name__)
+
+def bokeh_app(doc):
     df = pd.DataFrame({'x': [0,1,2,3,4,6],
                        'y': [0,1,2,3,4,5]})
 
@@ -24,15 +33,31 @@ def bkapp(doc):
     doc.add_root(column(plot))
     #doc.theme = Theme(filename="theme.yaml")
 
-@app.route('/bokeh', methods=['GET'])
-def bkapp_page():
+def plotly_app():
+    df = pd.DataFrame({'x': [1,2,5,6,7,8],
+                       'y': [1,2,5,6,7,8],
+                       'z': [1,2,5,6,7,8]})
+    fig = px.scatter_3d(df,
+                        x='z', y='x', z='y',
+                        color_discrete_sequence=['black'],
+                        symbol_sequence=['circle'],
+                        hover_data=['x', 'y', 'z'],
+                        )
+    return fig.to_html(full_html=False)
+
+@app.route('/viz', methods=['GET'])
+def webapp_page():
     if ssh_tunnel:
-        ssh_path = 'http://localhost:{}/bkapp'
+        ssh_path = 'http://localhost:{}/bokeh_app'
     else:
-        ssh_path = 'http://llruicms01.in2p3.fr:{}/bkapp'
-    script = server_document(ssh_path.format(bokeh_port))
+        ssh_path = 'http://llruicms01.in2p3.fr:{}/bokeh_app'
+    script_bokeh = server_document(ssh_path.format(bokeh_port))
+
+    script_plotly = plotly_app() #not really an app, rather an html block
+
     return render_template('embed.html', relative_urls=False,
-                           script=script,
+                           script_bokeh=script_bokeh,
+                           script_plotly=script_plotly,
                            framework='Flask')
 
 def bk_worker():
@@ -40,7 +65,7 @@ def bk_worker():
         web_socket = 'localhost:{}'
     else:
         web_socket = 'llruicms01.in2p3.fr:{}'
-    server = Server({'/bkapp': bkapp}, io_loop=IOLoop(),
+    server = Server({'/bokeh_app': bokeh_app}, io_loop=IOLoop(),
                     port=bokeh_port,
                     allow_websocket_origin=[web_socket.format(flask_port)])
     server.start()
