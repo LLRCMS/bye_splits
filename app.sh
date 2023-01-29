aff9962e08d8f9337ba45b121699f85639fdaba8
bfontana <20703947+b-fontana@users.noreply.github.com>
2023-01-29 19:27:17 +0100

    minor

--- app.sh
+++ app.sh
@@ -1,2 +1,2 @@
 #python bye_splits/plot/join/app.py --flask_port 8010 --bokeh_port 8008
-bokeh serve bye_splits/plot/display/ --address 0.0.0.0 --port 8080 --allow-websocket-origin=tc-hgcal-event-display.app.cern.ch
+bokeh serve bye_splits/plot/display/ --address 0.0.0.0 --port 8080 --allow-websocket-origin=trigger-cells-hgcal-event-display.app.cern.ch
