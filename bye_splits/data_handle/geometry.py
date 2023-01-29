589f5159f2e10097eaa059bd0a1ebb0c6ddf5b15
bfontana <20703947+b-fontana@users.noreply.github.com>
2023-01-29 23:03:00 +0100

    minor

--- bye_splits/data_handle/geometry.py
+++ bye_splits/data_handle/geometry.py
@@ -33,7 +33,7 @@ class GeometryData(BaseData):
             self.var = common.dot_dict(cfg['varGeometry'])
 
         self.readvars = list(self.var.values())
-        self.readvars.remove(self.varwvs)
+        self.readvars.remove(self.var.wvs)
         self.readvars.remove(self.var.c)
 
         self.cu, self.cv = 'triggercellu', 'triggercellv'
@@ -289,7 +289,7 @@ class GeometryData(BaseData):
             #data = data.drop_duplicates(subset=[self.var.cu, self.var.cv, self.var.l])
             data[self.var.wv] = data.waferv
             data[self.var.wvs] = -1 * data.waferv
-            #data[self.var.c] = "#8a2be2"
+            data[self.var.c] = "#8a2be2"
 
         return data
 
