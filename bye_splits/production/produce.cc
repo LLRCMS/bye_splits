50769ae12a610f373065f25cf25405be5d0583a4
Isaac Ehle <ehle@llr.in2p3.fr>
2023-03-24 13:10:47 +0100

    dR thresh between TCs and gen; YAML config for produce
    single match function for all RVec types
    
    Further YAML configs
    
    Default inpath set in YAML config file

--- bye_splits/production/produce.cc
+++ bye_splits/production/produce.cc
@@ -1,22 +1,26 @@
 #include <iostream>
+#include <tuple>
 #include "include/skim.h"
 
 #include <stdio.h>  // for printf()
 #include <stdlib.h> // for strtol()
 #include <errno.h>  // for errno
 #include <limits.h> // for INT_MIN and INT_MAX
-#include <string.h>  // for strlen
+#include <string.h> // for strlen
 
-int convert_to_int(char** argv, int idx) {
-  char* p;
+int convert_to_int(char **argv, int idx)
+{
+  char *p;
   errno = 0; // not 'int errno', because the '#include' already defined it
   long arg = strtol(argv[idx], &p, 10);
-  if (*p != '\0' || errno != 0) {
-	return 1; // In main(), returning non-zero means failure
+  if (*p != '\0' || errno != 0)
+  {
+    return 1; // In main(), returning non-zero means failure
   }
 
-  if (arg < INT_MIN || arg > INT_MAX) {
-	return 1;
+  if (arg < INT_MIN || arg > INT_MAX)
+  {
+    return 1;
   }
   int arg_int = arg;
 
@@ -24,71 +28,151 @@ int convert_to_int(char** argv, int idx) {
   return arg_int;
 }
 
-void show_help(const po::options_description&, const std::string&);
+void show_help(const po::options_description &, const std::string &);
 po::variables_map process_program_options(int argc, char **argv);
 
-void validate(po::variables_map args) {
+/*
+void validate(po::variables_map args)
+{
   std::string particles = args["particles"].as<string>();
-  if(!(particles=="photons" || particles=="electrons" || particles=="pions")) {
+  if (!(particles == "photons" || particles == "electrons" || particles == "pions"))
+  {
     throw po::validation_error(po::validation_error::invalid_option_value, "particles");
   }
 }
+*/
 
-void show_help(const po::options_description& desc,
-			   const std::string& topic = "") {
+void show_help(const po::options_description &desc,
+               const std::string &topic = "")
+{
   std::cout << desc << '\n';
-  if (topic != "") {
-	std::cout << "You asked for help on: " << topic << '\n';
+  if (topic != "")
+  {
+    std::cout << "You asked for help on: " << topic << '\n';
   }
 }
 
 po::variables_map process_program_options(int argc, char **argv)
 {
   po::options_description desc("Usage");
-  desc.add_options()
-	("help,h",
-	 po::value<string>()->implicit_value("")
-	 ->notifier([&desc](const std::string &topic) {show_help(desc, topic);}),
-	 "Show help. If given, show help on the specified topic.")
-	("nevents", po::value<int>()->default_value(-1),
-	 "number of entries to consider, useful for debugging (-1 means all)")
-	("particles", po::value<string>()->required(),
-	 "type of particle");
-  
-  if (argc <= 1) {
-	show_help(desc); // does not return
-	exit( EXIT_SUCCESS );
+  desc.add_options()("help,h",
+                     po::value<string>()->implicit_value("")->notifier([&desc](const std::string &topic)
+                                                                       { show_help(desc, topic); }),
+                     "Show help. If given, show help on the specified topic.")("nevents", po::value<int>()->default_value(-1),
+                                                                               "number of entries to consider, useful for debugging (-1 means all)")("inpath", po::value<string>()->implicit_value(""),
+                                                                                                                                                     "full path to ROOT file");
+  /*
+  if (argc <= 1)
+  {
+    show_help(desc); // does not return
+    exit(EXIT_SUCCESS);
   }
+  */
 
   po::variables_map args;
-  try {
-	po::store(po::parse_command_line(argc, argv, desc), args);
+  try
+  {
+    po::store(po::parse_command_line(argc, argv, desc), args);
   }
-  catch (po::error const& e) {
-	std::cerr << e.what() << '\n';
-	exit( EXIT_FAILURE );
+  catch (po::error const &e)
+  {
+    std::cerr << e.what() << '\n';
+    exit(EXIT_FAILURE);
   }
   po::notify(args);
-  validate(args);
+  // validate(args);
   return args;
 }
 
-//Run with ./produce.exe photons
-int main(int argc, char **argv) {
-  std::string dir = "/eos/user/b/bfontana/FPGAs/new_algos/";
-  std::string tree_name = "FloatingpointMixedbcstcrealsig4DummyHistomaxxydr015GenmatchGenclustersntuple/HGCalTriggerNtuple";
+tuple<string, string, string, string> read_config(YAML::Node config)
+{
+  string root_dir = "", root_tree = "", input_path = "", out_dir = "", outfile_base_name = "";
+  if (config["io"]["dir"])
+  {
+    root_dir = config["io"]["dir"].as<string>();
+  }
+  if (config["io"]["tree"])
+  {
+    root_tree = config["io"]["tree"].as<string>();
+  }
+  if (config["skim"]["infilePath"])
+  {
+    input_path = config["skim"]["infilePath"].as<string>();
+  }
+  if (config["skim"]["dir"])
+  {
+    out_dir = config["skim"]["dir"].as<string>();
+  }
+  if (config["skim"]["outfileBaseName"])
+  {
+    outfile_base_name = config["skim"]["outfileBaseName"].as<string>();
+  }
+  string tree_name = root_dir + "/" + root_tree;
+  return make_tuple(tree_name, input_path, out_dir, outfile_base_name);
+}
+
+string get_particles(string inpath)
+{
+  std::string phot_str = "photon";
+  std::string el_str = "electron";
+  std::string pi_str = "pion";
+  std::string particles;
+  if (inpath.find(phot_str) != std::string::npos)
+  {
+    particles = "photons";
+  }
+  else if (inpath.find(el_str) != std::string::npos)
+  {
+    particles = "electrons";
+  }
+  else if (inpath.find(pi_str) != std::string::npos)
+  {
+    particles = "pions";
+  }
+  else
+  {
+    std::cout << "\nInpath: " << inpath << "\n";
+    std::cout << "\nSub string: " << phot_str << "\n";
+  }
+  return particles;
+}
+
+// Run with ./produce.exe --inpath </full/path/to/file.root>
+int main(int argc, char **argv)
+{
+  // read input parameters
+  YAML::Node config = YAML::LoadFile("bye_splits/production/prod_params.yaml");
+
+  string tree_name, input_path, out_dir, outfile_base_name;
+  tie(tree_name, input_path, out_dir, outfile_base_name) = read_config(config);
 
   po::variables_map args = process_program_options(argc, argv);
-  if (args.count("help")) {
+  if (args.count("help"))
+  {
     return 1;
   }
+  string inpath;
+  if (args.count("inpath"))
+  {
+    inpath = args["inpath"].as<string>();
+  }
+  else
+  {
+    inpath = input_path;
+  }
 
-  string particles = args["particles"].as<string>();
+  // string inpath = args["inpath"].as<string>();
   int nevents = args["nevents"].as<int>();
 
-  std::string infile = particles + "_0PU_bc_stc_hadd.root";
+  string particles = get_particles(inpath);
+  out_dir += particles + "/new_skims/";
+
+  // Grab the file basename from the /full/path
+  std::string infile = inpath.substr(inpath.find_last_of("/\\") + 1);
   std::string events_str = nevents > 0 ? std::to_string(nevents) + "events_" : "";
-  std::string outfile = "skim_" + events_str + infile;
-  skim(tree_name, dir + infile, dir + outfile, particles, nevents);
+  std::string outfile = outfile_base_name + "_" + events_str + infile;
+  std::string outpath = out_dir + outfile;
+
+  skim(tree_name, inpath, outpath, particles, nevents);
   return 0;
 }
--- bye_splits/production/src/skim.cc
+++ bye_splits/production/src/skim.cc
@@ -1,97 +1,158 @@
+#include <vector>
+
 #include "include/skim.h"
 
 ROOT::VecOps::RVec<float> calcDeltaR(ROOT::VecOps::RVec<float> geta, ROOT::VecOps::RVec<float> gphi,
-									 ROOT::VecOps::RVec<float> cleta,  ROOT::VecOps::RVec<float> clphi)
+                                     ROOT::VecOps::RVec<float> cleta, ROOT::VecOps::RVec<float> clphi)
 {
-  if(geta.size()==0) // empty event (filtered before)
-	return ROOT::VecOps::RVec<float>();
+  if (geta.size() == 0) // empty event (filtered before)
+    return ROOT::VecOps::RVec<float>();
 
-  assert(geta.size() == 1); //consistency check
+  assert(geta.size() == 1); // consistency check
   unsigned ncl = cleta.size();
   ROOT::VecOps::RVec<float> deltaR(ncl);
   float deta, dphi;
-  
-  for(unsigned j=0; j<ncl; ++j) {
-	deta = fabs(cleta[j] - geta[0]);
-	dphi = fabs(clphi[j] - gphi[0]);
-	if(dphi > M_PI) dphi -= (2 * M_PI);
-	deltaR[j] = sqrtf(dphi*dphi + deta*deta);
+
+  for (unsigned j = 0; j < ncl; ++j)
+  {
+    deta = fabs(cleta[j] - geta[0]);
+    dphi = fabs(clphi[j] - gphi[0]);
+    if (dphi > M_PI)
+      dphi -= (2 * M_PI);
+    deltaR[j] = sqrtf(dphi * dphi + deta * deta);
   }
 
   return deltaR;
 }
 
-ROOT::RDF::RResultPtr<long long unsigned> addProgressBar(ROOT::RDF::RNode df) {
+template<typename T>
+std::vector<T> tcMatch(std::vector<T> tc_col, ROOT::VecOps::RVec<int> tc_matches)
+{
+  std::vector<T> matched_tcs;
+  unsigned tc_size = tc_col.size();
+  unsigned tc_match_size = tc_matches.size();
+  for (unsigned i = 0; i < tc_size; ++i)
+  {
+    if (i < tc_match_size && tc_matches[i] == 1)
+    {
+      matched_tcs.push_back(tc_col[i]);
+    }
+  }
+  return matched_tcs;
+}
+
+ROOT::RDF::RResultPtr<long long unsigned> addProgressBar(ROOT::RDF::RNode df)
+{
   auto c = df.Count();
   c.OnPartialResult(/*every=*/100,
-					[] (long long unsigned e) { cout << "Progress: " << e << endl; });
+                    [](long long unsigned e)
+                    { cout << "Progress: " << e << endl; });
   return c;
 }
 
-void skim(string tn, string inf, string outf, string particle, int nevents) {
+void skim(string tn, string inf, string outf, string particle, int nevents)
+{
 
   // read input parameters
   YAML::Node config = YAML::LoadFile("bye_splits/production/prod_params.yaml");
   vector<int> discLayers;
-  if (config["selection"]["disconnectedTriggerLayers"]) {
-	discLayers = config["selection"]["disconnectedTriggerLayers"].as<vector<int>>();
+  if (config["selection"]["disconnectedTriggerLayers"])
+  {
+    discLayers = config["selection"]["disconnectedTriggerLayers"].as<vector<int>>();
   }
-  string reachedEE="", deltarThreshold="", mipThreshold="";
+  string reachedEE = "", deltarThreshold = "", mipThreshold = "";
   if (config["selection"]["reachedEE"])
-	reachedEE = config["selection"]["reachedEE"].as<string>();
+    reachedEE = config["selection"]["reachedEE"].as<string>();
   if (config["selection"]["deltarThreshold"])
-	deltarThreshold = config["selection"]["deltarThreshold"].as<string>();
+    deltarThreshold = config["selection"]["deltarThreshold"].as<string>();
   if (config["selection"]["mipThreshold"])
-	mipThreshold = config["selection"]["mipThreshold"].as<string>();
+    mipThreshold = config["selection"]["mipThreshold"].as<string>();
+  
+  string tcDeltaRThresh = "";
+  if (config["skim"]["tcDeltaRThresh"])
+  {
+    tcDeltaRThresh = config["skim"]["tcDeltaRThresh"].as<string>();
+  }
   // variables
   string vtmp = "tmp_good";
-  
-  if(nevents==-1) { //RDataFrame.Range() does not work with multithreading
-	ROOT::EnableImplicitMT();
-	cout << "Multithreaded..." << endl;
+
+  if (nevents == -1)
+  { // RDataFrame.Range() does not work with multithreading
+    ROOT::EnableImplicitMT();
+    cout << "Multithreaded..." << endl;
   }
   ROOT::RDataFrame dataframe(tn, inf);
-  
+
   // gen-related variables
   vector<string> gen_intv = {"genpart_pid"};
   vector<string> gen_floatv = {"genpart_exphi", "genpart_exeta", "genpart_energy"};
-  vector<string> gen_floatv2 = {"genpart_posx", "genpart_posy", "genpart_posz"};
-  vector<string> gen_v = join_vars(gen_intv, gen_floatv, gen_floatv2);
+  // vector<string> gen_floatv2 = {"genpart_posx", "genpart_posy", "genpart_posz"};
+  // vector<string> gen_v = join_vars(gen_intv, gen_floatv, gen_floatv2);
+  vector<string> gen_v = join_vars(gen_intv, gen_floatv);
 
   // selection on generated particles (within each event)
-  unordered_map<string,string> pmap = {
-	{"photons", "22"},
-	{"electrons", "11"},
-	{"pions", "211"}};
+  unordered_map<string, string>
+      pmap = {
+          {"photons", "22"},
+          {"electrons", "11"},
+          {"pions", "211"}};
   string condgen = "genpart_gen != -1 && ";
   condgen += "genpart_reachedEE == " + reachedEE;
   condgen += " && genpart_pid == abs(" + pmap[particle] + ")";
   condgen += " && genpart_exeta > 0";
 
   auto df = dataframe.Define(vtmp + "_gens", condgen);
-  for(auto& v : gen_v) { df = df.Define(vtmp + "_" + v, v + "[" + vtmp + "_gens]"); }
+  for (auto &v : gen_v)
+  {
+    df = df.Define(vtmp + "_" + v, v + "[" + vtmp + "_gens]");
+  }
 
-  //remove events with zero generated particles
+  // remove events with zero generated particles
   auto dfilt = df.Filter(vtmp + "_genpart_pid.size()!=0");
 
   // trigger cells-related variables
-  vector<string> tc_uintv = {"tc_cluster_id"};
+  vector<string> tc_uintv = {"tc_multicluster_id"};
   vector<string> tc_intv = {"tc_layer", "tc_cellu", "tc_cellv", "tc_waferu", "tc_waferv"};
-  vector<string> tc_floatv = {"tc_energy", "tc_mipPt", "tc_pt", 
-							  "tc_x", "tc_y", "tc_z", "tc_phi", "tc_eta"};
+  vector<string> tc_floatv = {"tc_energy", "tc_mipPt", "tc_pt",
+                              "tc_x", "tc_y", "tc_z", "tc_phi", "tc_eta"};
 
   // selection on trigger cells (within each event)
   vector<string> tc_v = join_vars(tc_uintv, tc_intv, tc_floatv);
   string condtc = "tc_zside == 1 && tc_mipPt > " + mipThreshold + " && tc_layer <= 28";
   auto dd1 = dfilt.Define(vtmp + "_tcs", condtc);
-  for(auto& v : tc_v)
-	dd1 = dd1.Define(vtmp + "_" + v, v + "[" + vtmp + "_tcs]");
+  for (auto &v : tc_v)
+    dd1 = dd1.Define(vtmp + "_" + v, v + "[" + vtmp + "_tcs]");
+
+  // tighter tc selection, applying a deltaR threshold between the cells and gens
+  vector<string> tc_matchvars = {"tc_deltaR", "tc_matches"};
+  string tc_deltaR = tc_matchvars[0] + " <= " + tcDeltaRThresh;
+  dd1 = dd1.Define(tc_matchvars[0], calcDeltaR, {vtmp + "_genpart_exeta", vtmp + "_genpart_exphi", vtmp + "_tc_eta", vtmp + "_tc_phi"}).Define(tc_matchvars[1], tc_deltaR);
+
+  vector<string> tc_cut_uints = {};
+  dd1 = dd1.Define("tc_multicluster_id_cut", tcMatch<unsigned>, {"tc_multicluster_id", "tc_matches"});
+  tc_cut_uints.push_back("tc_multicluster_id_cut");
+
+  vector<string> tc_cut_ints = {};
+  for (auto &v : tc_intv)
+  {
+    dd1 = dd1.Define(v + "_cut", tcMatch<int>, {v, "tc_matches"});
+    tc_cut_ints.push_back(v + "_cut");
+  }
+
+  vector<string> tc_cut_floats = {};
+  for (auto &v : tc_floatv)
+  {
+    dd1 = dd1.Define(v + "_cut", tcMatch<float>, {v, "tc_matches"});
+    tc_cut_floats.push_back(v + "_cut");
+  }
+
+  vector<string> tc_cut_v = join_vars(tc_cut_uints, tc_cut_ints, tc_cut_floats);
 
   // // module sums-related variables
   // vector<string> tsum_intv = {"ts_layer", "ts_waferu", "ts_waferv"};
   // vector<string> tsum_floatv = {"ts_energy", "ts_mipPt", "ts_pt"};
   // vector<string> tsum_v = join_vars(tsum_intv, tsum_floatv);
-  
+
   // // selection on module trigger sums (within each event)
   // string condtsum = "ts_zside == 1 && ts_mipPt > " + mipThreshold;
   // dd1 = dd1.Define(vtmp + "_tsum", condtsum);
@@ -106,75 +167,88 @@ void skim(string tn, string inf, string outf, string particle, int nevents) {
   // selection on clusters (within each event)
   string condcl = "cl3d_eta > 0";
   dd1 = dd1.Define(vtmp + "_cl", condcl);
-  for(auto& v : cl_v)
-	dd1 = dd1.Define(vtmp + "_" + v, v + "[" + vtmp + "_cl]");
+  for (auto &v : cl_v)
+    dd1 = dd1.Define(vtmp + "_" + v, v + "[" + vtmp + "_cl]");
 
-  //remove events with zero clusters
+  // remove events with zero clusters
   auto dfilt2 = dd1.Filter(vtmp + "_cl3d_id.size()!=0");
 
   // matching
   vector<string> matchvars = {"deltaR", "matches"};
   string cond_deltaR = matchvars[0] + " <= " + deltarThreshold;
-  auto dd2 = dfilt2.Define(matchvars[0], calcDeltaR, {vtmp + "_genpart_exeta", vtmp + "_genpart_exphi",
-													  vtmp + "_cl3d_eta", vtmp + "_cl3d_phi"})
-	.Define(matchvars[1], cond_deltaR);
+  auto dd2 = dfilt2.Define(matchvars[0], calcDeltaR, {vtmp + "_genpart_exeta", vtmp + "_genpart_exphi", vtmp + "_cl3d_eta", vtmp + "_cl3d_phi"})
+                 .Define(matchvars[1], cond_deltaR);
 
   // convert root vector types to vector equivalents (uproot friendly)
   // vector<string> intv = join_vars(gen_intv, tc_intv, tsum_intv);
   vector<string> intv = join_vars(gen_intv, tc_intv);
-  for(auto& var : intv) {
-	dd2 = dd2.Define("good_" + var,
-					 [](const ROOT::VecOps::RVec<int> &v) {
-					   return vector<int>(v.begin(), v.end());
-					 },
-					 {vtmp + "_" + var});
+  for (auto &var : intv)
+  {
+    dd2 = dd2.Define("good_" + var,
+                     [](const ROOT::VecOps::RVec<int> &v)
+                     {
+                       return vector<int>(v.begin(), v.end());
+                     },
+                     {vtmp + "_" + var});
   }
   vector<string> uintv = join_vars(cl_uintv, tc_uintv);
-  for(auto& var : uintv) {
+  for (auto &var : uintv)
+  {
     dd2 = dd2.Define("good_" + var,
-					 [](const ROOT::VecOps::RVec<unsigned> &v) {
-					   return vector<unsigned>(v.begin(), v.end());
-					 },
-					 {vtmp + "_" + var});
+                     [](const ROOT::VecOps::RVec<unsigned> &v)
+                     {
+                       return vector<unsigned>(v.begin(), v.end());
+                     },
+                     {vtmp + "_" + var});
   }
-  //vector<string> floatv = join_vars(gen_floatv, tc_floatv, cl_floatv, tsum_floatv);
+  // vector<string> floatv = join_vars(gen_floatv, tc_floatv, cl_floatv, tsum_floatv);
   vector<string> floatv = join_vars(gen_floatv, tc_floatv, cl_floatv);
-  for(auto& var : floatv) {
-	dd2 = dd2.Define("good_" + var,
-					 [](const ROOT::VecOps::RVec<float> &v) {
-					   return vector<float>(v.begin(), v.end());
-					 },
-					 {vtmp + "_" + var});
+  for (auto &var : floatv)
+  {
+    dd2 = dd2.Define("good_" + var,
+                     [](const ROOT::VecOps::RVec<float> &v)
+                     {
+                       return vector<float>(v.begin(), v.end());
+                     },
+                     {vtmp + "_" + var});
   }
+  /*
   vector<string> floatv2 = join_vars(gen_floatv2);
-  for(auto& var : floatv2) {
-	dd2 = dd2.Define("good_" + var,
-					 [](const ROOT::VecOps::RVec<vector<float>> &v) {
-					   vector<vector<float>> vec(v.size());
-					   for(unsigned i=0; i<v.size(); ++i) {
-						 vec[i] = vector<float>(v[i].begin(), v[i].end());
-					   }
-					   return vec;
-					 },
-					 {vtmp + "_" + var});
+  for (auto &var : floatv2)
+  {
+    dd2 = dd2.Define("good_" + var,
+                     [](const ROOT::VecOps::RVec<vector<float>> &v)
+                     {
+                       vector<vector<float>> vec(v.size());
+                       for (unsigned i = 0; i < v.size(); ++i)
+                       {
+                         vec[i] = vector<float>(v[i].begin(), v[i].end());
+                       }
+                       return vec;
+                     },
+                     {vtmp + "_" + var});
   }
-  
+  */
+
   // define stored variables (and rename some)
   // vector<string> allvars = join_vars(gen_v, tc_v, cl_v, tsum_v);
-  vector<string> allvars = join_vars(gen_v, tc_v, cl_v);
+  vector<string> allvars = join_vars(gen_v, cl_v);
   vector<string> good_allvars = {"event"};
   good_allvars.insert(good_allvars.end(), matchvars.begin(), matchvars.end());
-  for(auto& v : allvars)
-	good_allvars.push_back("good_" + v);
+  good_allvars.insert(good_allvars.end(), tc_cut_v.begin(), tc_cut_v.end());
+  for (auto &v : allvars)
+    good_allvars.push_back("good_" + v);
 
   // store skimmed file
-  if(nevents>0) {
-	dd2.Range(0, nevents).Snapshot(tn, outf, good_allvars);
+  if (nevents > 0)
+  {
+    dd2.Range(0, nevents).Snapshot(tn, outf, good_allvars);
   }
-  else {
-	dd2.Snapshot(tn, outf, good_allvars);
+  else
+  {
+    dd2.Snapshot(tn, outf, good_allvars);
   }
-  
+
   // display event processing progress
   ROOT::RDF::RResultPtr<long long unsigned> count = addProgressBar(ROOT::RDF::RNode(dd2));
   count.GetValue();
