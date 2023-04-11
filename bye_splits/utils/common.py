09efc26c6789feb82542d168446437efade369d7
Isaac Ehle <ehle@llr.in2p3.fr>
2023-04-11 10:37:56 +0200

    Rewrite job submission in terms of classes, remove some duplication

--- bye_splits/utils/common.py
+++ bye_splits/utils/common.py
@@ -14,6 +14,7 @@ import os
 import yaml
 import numpy as np
 import pandas as pd
+import re
 
 
 def binConv(vals, dist, amin):
@@ -115,3 +116,77 @@ def print_histogram(arr):
             else:
                 print("X", end="|")
         print()
+
+# Accepts a template for a full path to a file and increments the version
+def increment_version(file_path):
+    dir, file = os.path.split(file_path)
+    base, ext = os.path.splitext(file)
+    i = 0
+    file_path = "{}/{}_v{}{}".format(dir, base, i, ext)
+    while os.path.exists(file_path):
+        i += 1
+        file_path = "{}/{}_v{}{}".format(dir, base, i, ext)
+    return file_path
+
+
+# Grab the most recent version of the file corresponding to the template file_path (or return all matches)
+def grab_most_recent(file_path, return_all=False):
+    dir, file = os.path.split(file_path)
+    base, ext = os.path.splitext(file)
+    files = os.listdir(dir)
+    version_pattern = re.compile("{}_v(\\d+)\\{}".format(base, ext))
+    matches = [version_pattern.search(file) for file in files]
+    matches = [match for match in matches if not match is None]
+    if len(matches) > 0:
+        matches = [int(match.group(1)) for match in matches]
+        most_recent = max(matches)
+        file_path = dir + "/" + base + "_v" + str(most_recent) + ext
+        file_paths = [dir + "/" + base + "_v" + str(f) + ext for f in matches]
+        if not return_all:            
+            return file_path
+        else:
+            return file_paths
+    else:
+        return None
+
+def compare_file_contents(file_path, buffer_list):
+    """
+    Compares the content in <file_path> with <buffer_list>,
+    which should be a list of strings that you wish to write
+    to a new file.
+    """
+    with open(file_path, "r") as file:
+        contents = file.readlines()
+    return contents==buffer_list
+
+def write_file_version(template, version):
+    file_name = increment_version(template)
+    with open(file_name, "w") as job_file:
+        job_file.writelines(version)
+    st = os.stat(file_name)
+    os.chmod(file_name, st.st_mode | 0o744)
+    return file_name
+
+def conditional_write(file_versions, file_template, current_version):
+    """
+    Loops through the files in <file_versions>, comparing their contents
+    to the current version. If an identical version is found, the function
+    breaks and does nothing. Otherwise, it will write the contents in
+    <current_version> to an updated version number whose basename corresponds to
+    <file_template>.
+    """
+    if file_versions != None:
+        identical_version = False
+        for file in file_versions:
+            if not compare_file_contents(file, current_version):
+                continue
+            else:
+                identical_version = True
+                file_path = grab_most_recent(file_template)
+                break
+        if not identical_version:
+            file_path = write_file_version(file_template, current_version)
+    
+    else:
+        file_path = write_file_version(file_template, current_version)
+    return file_path
